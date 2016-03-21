7239d23cdb15f4afa049f95f1fe5654e3f9f13f4
TimNYC <timwnewyork@gmail.com>
2016-03-20 23:17:28 -0400

    add more jar

--- Web-Crawler/src/test/unit/core/URLTest.java
+++ Web-Crawler/src/test/unit/core/URLTest.java
@@ -3,8 +3,6 @@ package test.unit.core;
 /**
  * Created by Tim Wang on 3/20/2016.
  */
-//import sun.net.www.http.HttpClient;
-
 import java.io.IOException;
 import java.net.URL;
 import java.io.InputStream;
@@ -13,8 +11,8 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.*;
 import org.apache.http.util.EntityUtils;
 
@@ -73,7 +71,53 @@ public class URLTest {
 
     }
 
+    public static void ReleaseConnection() {
+        String url = "http://www.google.com";
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet(url);
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                System.out.println("----------------------------------------");
+                System.out.println(response.getStatusLine());
+                // Get hold of the response entity
+                HttpEntity entity = response.getEntity();
+
+                // If the response does not enclose an entity, there is no need
+                // to bother about connection release
+                if (entity != null) {
+                    InputStream instream = entity.getContent();
+                    try {
+                        System.out.println((char)instream.read());
+                        // do something useful with the response
+                    } catch (IOException ex) {
+                        // In case of an IOException the connection will be released
+                        // back to the connection manager automatically
+                        throw ex;
+                    } finally {
+                        // Closing the input stream will trigger connection release
+                        instream.close();
+                    }
+                }
+            } finally {
+                System.out.println("inner close");
+                //response.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            try {
+                System.out.println("outer close");
+                httpclient.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        getClient();
+        ReleaseConnection();
     }
 }
