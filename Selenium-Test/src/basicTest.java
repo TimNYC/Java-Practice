d8b942f3fe6a681cd443bdaa214f903efdbcd5ac
TimNYC <timwnewyork@gmail.com>
2016-04-10 16:12:34 -0400

    testBase added

--- Selenium-Test/src/basicTest.java
+++ Selenium-Test/src/basicTest.java
@@ -1,14 +1,20 @@
 /**
  * Created by Tim Wang on 4/3/2016.
  */
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 
 public class basicTest {
     public static void testChrome() {
@@ -19,14 +25,27 @@ public class basicTest {
         //System.out.println(driver.getPageSource());
     }
 
-    public static void testBase() {
-        WebDriver driver=null;
-
+    public static void testBase() throws IOException {
+        WebDriver driver = null;
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("Web-Crawler/properties/dataDriven.properties");
+        prop.load(fis);
+        if(prop.getProperty("browser").equals("firefox")) {
+            driver =new FirefoxDriver();
+        } else if(prop.getProperty("browser").equals("chrome")) {
+            driver =new ChromeDriver();
+        } else {
+            driver =new InternetExplorerDriver();
+        }
+        driver.get(prop.getProperty("url"));
+        //driver.findElement(By.xpath:').sendkeys(username)
+        System.out.println(prop);
+        //driver.findElement(By.xpath(":'")).sendKeys(prop.getProperty("username"));
     }
 
-    public static void main(String[] args) throws MalformedURLException{
-        testChrome();
-
+    public static void main(String[] args) throws MalformedURLException, IOException{
+        //testChrome();
+        //testBase();
 
     }
 }
