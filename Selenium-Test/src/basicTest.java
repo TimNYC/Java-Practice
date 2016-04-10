a0899b59555f396a447d0d4705a40bd4b1e2729e
TimNYC <timwnewyork@gmail.com>
2016-04-10 15:39:05 -0400

    selenium

--- Selenium-Test/src/basicTest.java
+++ Selenium-Test/src/basicTest.java
@@ -1,5 +1,28 @@
 /**
  * Created by Tim Wang on 4/3/2016.
  */
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class basicTest {
+    public static void testChrome() {
+        System.setProperty("webdriver.chrome.driver", "tools/chromedriver.exe");
+        WebDriver driver =new ChromeDriver();
+        driver.get("http://www.google.com");
+        System.out.println(driver.getTitle());
+        //System.out.println(driver.getPageSource());
+    }
+
+    public static void
+    public static void main(String[] args) throws MalformedURLException{
+        testChrome();
+
+
+    }
 }
