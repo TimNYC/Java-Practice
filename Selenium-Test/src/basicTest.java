f5dc283c0d722c0a3e3f01989a1802ce9477f444
TimNYC <timwnewyork@gmail.com>
2016-04-11 00:28:24 -0400

    remoteDriver added

--- Selenium-Test/src/basicTest.java
+++ Selenium-Test/src/basicTest.java
@@ -57,20 +57,27 @@ public class basicTest {
         Assert.assertEquals("Electronics, Cars, Fashion, Collectibles, Coupons and More | eBay", driver.getTitle());
     }
 
-
     public static void loginLinkedin() {
         WebDriver driver = new FirefoxDriver();
         driver.get("http://linkedin.com");
         driver.findElement(By.id("login-email")).sendKeys("123123");
         driver.findElement(By.id("login-password")).sendKeys("123123");
+        //driver.findElement(By.partialLinkText("forget password")).click();
         driver.findElement(By.xpath("//*[@id=\"pagekey-uno-reg-guest-home\"]/div[1]/div/form/input[6]")).click();
+    }
+
+    public static void CSSselector() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://google.com");
+        System.out.println(driver.findElement(By.cssSelector("#gsr > script")).getText());
+
     }
 
     public static void main(String[] args) throws IOException{
         //testChrome();
         //testBase();
         //remoteDriver();
-        loginLinkedin();
-
+        //loginLinkedin();
+        CSSselector();
     }
 }
